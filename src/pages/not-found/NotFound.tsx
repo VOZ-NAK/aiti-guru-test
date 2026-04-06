b21e860c784b73d6c86785f8ef97b5bd5a0782ad
Anastasiia Vozniak <voznyakanastassia@yandex.ru>
2026-04-06 17:19:21 +0700

    feat: add 404 Not Found page

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,4 +1,17 @@
+import { Typography } from '@/shared/ui';
+
+import styles from './not-found.module.scss';
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className={styles.notFound}>
+      <Typography variant="cairo-24">404</Typography>
+      <Typography variant="cairo-20">Страница не найдена</Typography>
+      <Typography variant="opensans-16">
+        Запрашиваемая страница не существует или была перемещена
+      </Typography>
+    </div>
+  );
 };
+
 export default NotFound;
